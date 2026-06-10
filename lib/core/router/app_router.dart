eed7955d67be515e3ebcb9ede9aafc95367f8fdc
victor.dev <joaovictorbezerra.dev@gmail.com>
2026-06-09 22:24:52 -0400

    feat(router): register spaces module routes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,6 +2,8 @@ import 'package:go_router/go_router.dart';
 import '../../modules/authenticate/presentation/auth_screen.dart';
 import '../../modules/host/presentation/host_screen.dart';
 import '../../modules/organizations/presentation/organization_details_screen.dart';
+import '../../modules/spaces/presentation/space_details_screen.dart';
+import '../../modules/spaces/presentation/spaces_screen.dart';
 import '../../modules/splash_screen/presentation/splash_screen.dart';
 
 final appRouter = GoRouter(
@@ -18,6 +20,17 @@ final appRouter = GoRouter(
       builder: (context, state) {
         final id = int.tryParse(state.pathParameters['id'] ?? '');
         return OrganizationDetailsScreen(organizationId: id ?? 0);
+      },
+    ),
+    GoRoute(
+      path: '/spaces',
+      builder: (context, state) => const SpacesScreen(),
+    ),
+    GoRoute(
+      path: '/spaces/:id',
+      builder: (context, state) {
+        final id = int.tryParse(state.pathParameters['id'] ?? '');
+        return SpaceDetailsScreen(spaceId: id ?? 0);
       },
     ),
   ],
